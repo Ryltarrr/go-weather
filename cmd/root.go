66047bb8a1625167fa7b2c1092b7128b781768ff
Justin Guerré <guerrejustin@gmail.com>
2022-04-02 10:21:25 +0200

    add description

--- cmd/root.go
+++ cmd/root.go
@@ -11,22 +11,14 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-weather",
-	Short: "Simple CLI weather app",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Simple weather CLI app",
+	Long: `go-weather is a simple CLI tool to get the weather of a french city`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		city := api.FindCity(args[0])
 		weather := api.GetWeather(city.Centre.Coordinates)
 
-		fmt.Printf("Actually in %v, the weather is %v and temperature is %v°C (min: %v, max: %v)",
+		fmt.Printf("Actually in %v, the weather is %v and temperature is %v°C (min: %v°C, max: %v°C)",
 			city.Nom, weather.Weather[0].Description, weather.Main.Temp, weather.Main.TempMin, weather.Main.TempMax)
 	},
 }
@@ -46,5 +38,4 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-weather.yaml)")
-
 }
